c3f8e26d6f495f85158753e385b3eac1aeb0179d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-09-27 12:36:08 -0700

    Update script.

--- tests/blend_export/blend_export.py
+++ tests/blend_export/blend_export.py
@@ -5,30 +5,30 @@ import shutil
 import os
 
 directory = "blend"
+export_type = "gltf"
+shutil.rmtree(export_type, ignore_errors=True)
+posixpath.os.mkdir(export_type, mode=0o777)
+export_type = "obj"
+shutil.rmtree(export_type, ignore_errors=True)
+posixpath.os.mkdir(export_type, mode=0o777)
+export_type = "fbx"
+shutil.rmtree(export_type, ignore_errors=True)
+posixpath.os.mkdir(export_type, mode=0o777)
+
 for filename in os.listdir(directory):
     if filename.endswith(".blend"):
         print(filename)
         bpy.ops.wm.open_mainfile(filepath="blend/" + filename)
-
         strip_file_name = filename.rsplit(".blend", 1)[0]
-
         export_type = "gltf"
-        shutil.rmtree(export_type, ignore_errors=True)
-        posixpath.os.mkdir(export_type, mode=0o777)
         bpy.ops.export_scene.gltf(
             filepath=export_type + "/" + strip_file_name + ".gltf",
             export_format="GLTF_SEPARATE",
             export_copyright="The MIT License (MIT) Copyright (c) 2016 Godot Engine",
         )
-
         export_type = "obj"
-        shutil.rmtree(export_type, ignore_errors=True)
-        posixpath.os.mkdir(export_type, mode=0o777)
         bpy.ops.export_scene.obj(filepath=export_type + "/" + strip_file_name + "." + export_type)
-
         export_type = "fbx"
-        shutil.rmtree(export_type, ignore_errors=True)
-        posixpath.os.mkdir(export_type, mode=0o777)
         bpy.ops.export_scene.fbx(filepath=export_type + "/" + strip_file_name + "." + export_type)
     else:
         continue
