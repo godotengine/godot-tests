d282d1f878017dce1b5a0b67b7425fc97bd66430
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-11-17 08:05:10 -0800

    Add test case for 43044
    Abstract python code.

--- tests/blend_export/blend_export.py
+++ tests/blend_export/blend_export.py
@@ -5,36 +5,23 @@ import shutil
 import os
 
 directory = "blend"
-export_type = "gltf"
-shutil.rmtree(export_type, ignore_errors=True)
-posixpath.os.mkdir(export_type, mode=0o777)
-export_type = "obj"
-shutil.rmtree(export_type, ignore_errors=True)
-posixpath.os.mkdir(export_type, mode=0o777)
-export_type = "fbx"
-shutil.rmtree(export_type, ignore_errors=True)
-posixpath.os.mkdir(export_type, mode=0o777)
-export_type = "dae"
-shutil.rmtree(export_type, ignore_errors=True)
-posixpath.os.mkdir(export_type, mode=0o777)
-
-
+export_types = ["gltf", "obj", "fbx"]
+for export_type in export_types:
+    shutil.rmtree(export_type, ignore_errors=True)
+    posixpath.os.mkdir(export_type, mode=0o777)
 for filename in os.listdir(directory):
     if filename.endswith(".blend"):
-        print(filename)
-        bpy.ops.wm.open_mainfile(filepath="blend/" + filename)
-        strip_file_name = filename.rsplit(".blend", 1)[0]
-        export_type = "gltf"
-        bpy.ops.export_scene.gltf(
-            filepath=export_type + "/" + strip_file_name + ".gltf",
-            export_format="GLTF_SEPARATE",
-            export_copyright="The MIT License (MIT) Copyright (c) 2020 Godot Engine",
-        )
-        export_type = "obj"
-        bpy.ops.export_scene.obj(filepath=export_type + "/" + strip_file_name + "." + export_type)
-        export_type = "fbx"
-        bpy.ops.export_scene.fbx(filepath=export_type + "/" + strip_file_name + "." + export_type)
-        export_type = "dae"
-        bpy.ops.export_scene.fbx(filepath=export_type + "/" + strip_file_name + "." + export_type)
-    else:
-        continue
+        for export_type in export_types:
+            print(filename)
+            bpy.ops.wm.open_mainfile(filepath="blend/" + filename)
+            strip_file_name = filename.rsplit(".blend", 1)[0]
+            if export_type == "gltf":
+                bpy.ops.export_scene.gltf(
+                    filepath=export_type + "/" + strip_file_name + ".gltf",
+                    export_format="GLTF_SEPARATE",
+                    export_copyright="The MIT License (MIT) Copyright (c) 2020 Godot Engine",
+                )
+            elif export_type == "fbx":
+                bpy.ops.export_scene.fbx(filepath=export_type + "/" + strip_file_name + "." + export_type)
+            elif export_type == "obj":
+                bpy.ops.export_scene.obj(filepath=export_type + "/" + strip_file_name + "." + export_type)
